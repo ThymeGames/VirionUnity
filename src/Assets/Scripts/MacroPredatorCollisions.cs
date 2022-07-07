326ed2b7fbd62cf07e66c306da336ba9bd8cb1b0
Andrey Pikunov <pikunov@phystech.edu>
2022-07-07 19:55:03 +0400

    darkness

--- src/Assets/Scripts/MacroPredatorCollisions.cs
+++ src/Assets/Scripts/MacroPredatorCollisions.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MacroPredatorCollisions : MonoBehaviour
 {
 
-    Animator anim;
     public GameObject darkness;
+    public float death_time = 5f;
+
+    Animator anim;
+    float timer = 0;
+    float scene_time = 100000f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +22,11 @@ public class MacroPredatorCollisions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer > scene_time) {
+            LoadMenu();
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -30,6 +39,13 @@ public class MacroPredatorCollisions : MonoBehaviour
             Instantiate(darkness);
             gameObject.GetComponent<Rigidbody2D>().velocity *= 0f;
             anim.Play("Run", -1, 0f);
+
+            scene_time = timer + death_time;
         }
     }
+
+    void LoadMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
 }
